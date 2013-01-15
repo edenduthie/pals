f4452f998df576db911d8705756b404623285b63
Eden Duthie <eduthie@gmail.com>
2013-01-15 16:30:38 +1100

    modified to find error code and truncated R script name without the full path

--- palsweb/src/pals/utils/PalsFileUtils.java
+++ palsweb/src/pals/utils/PalsFileUtils.java
@@ -8,10 +8,13 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.log4j.Logger;
 
@@ -24,191 +27,321 @@ import pals.entity.DataSetVersion;
 import pals.entity.ModelOutput;
 import pals.entity.User;
 
-
-public class PalsFileUtils {
-	
+public class PalsFileUtils
+{
 	private static Logger log = Logger.getLogger(PalsFileUtils.class);
-	
-	public static void copyFileInJava(String srcPath, String targetPath) throws IOException {
-		log.info("Copying file " +srcPath+ " to " + targetPath);
-		FileInputStream in = null;
-		FileOutputStream out = null;  
-		File userFile = new File ( targetPath);
-		File srcFile = new File( srcPath);
-		try {
-	            in = new FileInputStream( srcFile );
-	            out = new FileOutputStream( userFile );
-	            int c;
+	private static List<String> ERROR_CODES = Configuration.getInstance().ERROR_CODES;
 
-	            while ((c = in.read()) != -1) {
-	                out.write(c);
-	            }
-		} finally {
-	            if (in != null) {
-	                in.close();
-	            }
-	            if (out != null) {
-	                out.close();
-	            }
+	public static void copyFileInJava(String srcPath, String targetPath)
+			throws IOException
+	{
+		log.info("Copying file " + srcPath + " to " + targetPath);
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		File userFile = new File(targetPath);
+		File srcFile = new File(srcPath);
+		try
+		{
+			in = new FileInputStream(srcFile);
+			out = new FileOutputStream(userFile);
+			int c;
+
+			while ((c = in.read()) != -1)
+			{
+				out.write(c);
+			}
+		}
+		finally
+		{
+			if (in != null)
+			{
+				in.close();
+			}
+			if (out != null)
+			{
+				out.close();
+			}
 		}
 	}
-	
-	public static void executeCommand(String exStr) throws AnalysisException, IOException, InterruptedException {
-		log.info("Executing command: " + exStr);
-		CommandLine commandLine = CommandLine.parse(exStr);
+
+	/**
+	 * Executes a shell command using Apache Commons Exec.
+	 * By setting the exit value, it throws an exception
+	 * stderr only reports that the process exited with non-default value.
+	 * That is not useful and the stdout contains the custom error
+	 * message from  stop().
+	 * Using R --vanilla --slave, the message from the R interactive
+	 * mode can be suppressed and only the custom error message will
+	 * be present.
+	 * @param cmdStr
+	 * @throws AnalysisException
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public static void executeCommand(String cmdStr) throws AnalysisException,
+			IOException, InterruptedException
+	{
+		log.info("Executing command: " + cmdStr);
+		String cmd = findRScriptName(cmdStr);
+		
+		CommandLine commandLine = CommandLine.parse(cmdStr);
 		DefaultExecutor executor = new DefaultExecutor();
-		executor.setExitValue(Configuration.getInstance().DEFAULT_EXIT_CODE);
+		int normalExitVal = Configuration.getInstance().DEFAULT_EXIT_CODE;
+		executor.setExitValue(normalExitVal);
 		ByteArrayOutputStream stdout = new ByteArrayOutputStream();
-        PumpStreamHandler psh = new PumpStreamHandler(stdout);
-        executor.setStreamHandler(psh);
-        try
-        {
-		    int exitValue = executor.execute(commandLine);
-		    log.debug("Runtime call exit value was:" + exitValue);
-        }
-        catch( IOException e )
-        {
-        	throw new AnalysisException(parseOutputForError(stdout,exStr));
-        }
-	}
-	
-	public static String parseOutputForError(ByteArrayOutputStream output, String command) throws IOException
-	{	
-		String error = "R script failure script: " + command;
-		ByteArrayInputStream in = new ByteArrayInputStream(output.toByteArray());
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		String line;
-		while( (line = reader.readLine()) != null)
+		PumpStreamHandler psh = new PumpStreamHandler(stdout);
+		executor.setStreamHandler(psh);
+		//String errMsg = null;
+		int exitValue = normalExitVal;
+		// exitValue is not used to detect an error, because
+		// setExitValue() makes execute() to throw an exception
+		// when the exitValue is not the set value.
+		try
 		{
-			for( String code : Configuration.getInstance().ERROR_CODES )
+			exitValue = executor.execute(commandLine);
+		}
+		catch (ExecuteException e)
+		{
+			//errMsg = e.getMessage();
+			//log.error(errMsg);
+			throw new AnalysisException(parseOutputForError(stdout, cmd));
+		}
+		catch (IOException e)
+		{
+			//errMsg = e.getMessage();
+			//log.error(errMsg);
+			throw new IOException(e.getCause().getMessage());
+		}
+		finally
+		{
+			// Maybe the executor can clean up itself - e.g., stopping threads and closing streams
+		}
+	}
+
+	/**
+	 * Find what R command was run
+	 * @param cmdStr
+	 * @return
+	 */
+	private static String findRScriptName(String cmdStr)
+	{
+		String cmd = cmdStr;
+		//String[] cmdList = cmdStr.split("\b");
+		//Pattern p = Pattern.compile("\b*([^\b]*[.]{1}+R)\b.*$");
+		Pattern p = Pattern.compile("([^/]*.R)");
+		Matcher m = p.matcher(cmdStr);
+		if (m.find())
+			cmd = m.group(1);
+
+		return cmd;
+	}
+
+	/**
+	 * Filtering out the error message. 
+	 * cmdStr is not used, because it is a part of
+	 * the error message returned from R.
+	 * If "Error" is not found in stdout, the whole
+	 * stdout is returned.
+	 * @param stdout
+	 * @param cmd
+	 * @return
+	 * @throws IOException
+	 */
+	public static String parseOutputForError(ByteArrayOutputStream stdout,
+			String cmd) throws IOException
+	{
+		String errMsg = stdout.toString(); // default return value
+		
+		ByteArrayInputStream in = new ByteArrayInputStream(stdout.toByteArray());
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		String line = null;
+		String foundErr = null;
+		while ((line = reader.readLine()) != null)
+		{
+			// expecting only one Error line
+			if (line.contains("Error"))
 			{
-				if( line.contains(code) )
+				for (String code: ERROR_CODES)
 				{
-					log.debug("Error code picked up: " + code);
-					String[] splitLine = line.split(":");
-					if( splitLine.length >= 2 )
+					if (line.contains(code))
 					{
-						error = splitLine[1].trim();
-						for( int i=2; i < splitLine.length; ++i )
-						{
-							error += ":" + splitLine[i];
-						}
-						// just make sure it is only one sentence
-						String[] sentences = error.split("\\^");
-						if( sentences.length > 0 )
-						{
-							error = sentences[0].trim();
-						}
+						foundErr = "Error: " + cmd + " (" + code + ")";
+						// this could include description of the error code
+						break;
 					}
+				}
+				
+				if(foundErr != null)
+				{
+					log.debug(line);
+					break;
 				}
 			}
 		}
-		return error;
+		if(foundErr != null) 
+		{
+			return  foundErr;
+		}
+		else
+		{
+			return errMsg; 
+		}
 	}
-	
-	public static String getUserDirPath(User user) {
-		return Configuration.getInstance().PATH_TO_APP_DATA + "/" + user.getUsername();
+
+	public static String getUserDirPath(User user)
+	{
+		return Configuration.getInstance().PATH_TO_APP_DATA + "/"
+				+ user.getUsername();
 	}
-	
-	private static String getModelOutputDirPath(Integer modelOutputId, User user) {
-		//return getUserDirectory(userName) + File.separator + Globals.MODEL_OUTPUT_DIR_PREFIX + modelOutputId;
+
+	private static String getModelOutputDirPath(Integer modelOutputId, User user)
+	{
+		// return getUserDirectory(userName) + File.separator +
+		// Globals.MODEL_OUTPUT_DIR_PREFIX + modelOutputId;
 		return getUserDirPath(user);
 	}
-	
-	public static String getModelOutputDirPath(ModelOutput modelOutput) {
-		return getModelOutputDirPath(modelOutput.getId(), modelOutput.getOwner());
+
+	public static String getModelOutputDirPath(ModelOutput modelOutput)
+	{
+		return getModelOutputDirPath(modelOutput.getId(),
+				modelOutput.getOwner());
 	}
-	
-	public static String getModelOutputFilePathWithSuffix(Integer modelOutputId, User user, String suffix) {
-		return getModelOutputDirPath(modelOutputId, user) + File.separator + Globals.MODEL_OUTPUT_FILE_PREFIX + modelOutputId + suffix;
+
+	public static String getModelOutputFilePathWithSuffix(
+			Integer modelOutputId, User user, String suffix)
+	{
+		return getModelOutputDirPath(modelOutputId, user) + File.separator
+				+ Globals.MODEL_OUTPUT_FILE_PREFIX + modelOutputId + suffix;
 	}
-	
-	private static String getModelOutputFilePath(Integer modelOutputId, User user) {
-		return getModelOutputFilePathWithSuffix(modelOutputId, user, Globals.NETCDF_FILE_SUFFIX);
+
+	private static String getModelOutputFilePath(Integer modelOutputId,
+			User user)
+	{
+		return getModelOutputFilePathWithSuffix(modelOutputId, user,
+				Globals.NETCDF_FILE_SUFFIX);
 	}
-	
-	public static String getModelOutputFilePath(ModelOutput modelOutput) {
-		return getModelOutputFilePath(modelOutput.getId(), modelOutput.getOwner());
+
+	public static String getModelOutputFilePath(ModelOutput modelOutput)
+	{
+		return getModelOutputFilePath(modelOutput.getId(),
+				modelOutput.getOwner());
 	}
-	
-	public static String getDataSetDirPath(DataSet dataSet) {
-		//return Configuration.getInstance().PATH_TO_APP_DATA + File.separator + dataSet.getUserName() + File.separator + Globals.DATA_SET_DIR_PREFIX + dataSet.getDataSetId(); 
+
+	public static String getDataSetDirPath(DataSet dataSet)
+	{
+		// return Configuration.getInstance().PATH_TO_APP_DATA + File.separator
+		// + dataSet.getUserName() + File.separator +
+		// Globals.DATA_SET_DIR_PREFIX + dataSet.getDataSetId();
 		return getUserDirPath(dataSet.getOwner());
 	}
-	
-	public static String getDataSetVersionFilePathWidthSuffix(DataSetVersion dataSetVersion, String suffix) {
-		return getDataSetDirPath(dataSetVersion.getDataSet()) + "/" + Globals.DATA_SET_FILE_PREFIX + dataSetVersion.getDataSet().getId() + "." + dataSetVersion.getId() + suffix;
+
+	public static String getDataSetVersionFilePathWidthSuffix(
+			DataSetVersion dataSetVersion, String suffix)
+	{
+		return getDataSetDirPath(dataSetVersion.getDataSet()) + "/"
+				+ Globals.DATA_SET_FILE_PREFIX
+				+ dataSetVersion.getDataSet().getId() + "."
+				+ dataSetVersion.getId() + suffix;
 	}
-	
-	public static String getDataSetVersionFluxFilePath(DataSetVersion dataSetVersion) {
-		return getDataSetVersionFilePathWidthSuffix(dataSetVersion, "_flux" + Globals.NETCDF_FILE_SUFFIX);
+
+	public static String getDataSetVersionFluxFilePath(
+			DataSetVersion dataSetVersion)
+	{
+		return getDataSetVersionFilePathWidthSuffix(dataSetVersion, "_flux"
+				+ Globals.NETCDF_FILE_SUFFIX);
 	}
-	
-	public static String getDataSetVersionMetFilePath(DataSetVersion dataSetVersion) {
-		return getDataSetVersionFilePathWidthSuffix(dataSetVersion, "_met" + Globals.NETCDF_FILE_SUFFIX);
+
+	public static String getDataSetVersionMetFilePath(
+			DataSetVersion dataSetVersion)
+	{
+		return getDataSetVersionFilePathWidthSuffix(dataSetVersion, "_met"
+				+ Globals.NETCDF_FILE_SUFFIX);
 	}
-	
-	public static File getDataSetVersionMetFile(DataSetVersion dataSetVersion) {
+
+	public static File getDataSetVersionMetFile(DataSetVersion dataSetVersion)
+	{
 		return new File(getDataSetVersionMetFilePath(dataSetVersion));
 	}
-	
-	public static File getDataSetVersionFluxFile(DataSetVersion dataSetVersion) {
+
+	public static File getDataSetVersionFluxFile(DataSetVersion dataSetVersion)
+	{
 		return new File(getDataSetVersionFluxFilePath(dataSetVersion));
 	}
-	
-	public static String getDataSetVersionBenchmarkFilePath(DataSetVersion dataSetVersion) {
-		return getDataSetVersionFilePathWidthSuffix(dataSetVersion, "_bench" + Globals.NETCDF_FILE_SUFFIX);
+
+	public static String getDataSetVersionBenchmarkFilePath(
+			DataSetVersion dataSetVersion)
+	{
+		return getDataSetVersionFilePathWidthSuffix(dataSetVersion, "_bench"
+				+ Globals.NETCDF_FILE_SUFFIX);
 	}
-	
-	/* OLD METHOD
+
+	/*
+	 * OLD METHOD
 	 * 
 	 * public static String getAnalysisRunFileLabel(AnalysisRun analRun) {
-		String fileName = Globals.MODEL_OUTPUT_FILE_PREFIX + analRun.getModelOutputId() + Globals.ANALYSIS_RUN_FILE_PREFIX + analRun.getAnalysisId();
-		return getModelOutputDirPath(analRun.getModelOutputId(), analRun.getModelOutput().getUser()) + File.separator + fileName;
-	}*/
-	
-	public static String getAnalysisRunFileLabel(Analysis analRun) {
-		//String fileName = Globals.MODEL_OUTPUT_FILE_PREFIX + analRun.getAnalysisRunId() + Globals.ANALYSIS_RUN_FILE_PREFIX + analRun.getAnalysisId();
+	 * String fileName = Globals.MODEL_OUTPUT_FILE_PREFIX +
+	 * analRun.getModelOutputId() + Globals.ANALYSIS_RUN_FILE_PREFIX +
+	 * analRun.getAnalysisId(); return
+	 * getModelOutputDirPath(analRun.getModelOutputId(),
+	 * analRun.getModelOutput().getUser()) + File.separator + fileName; }
+	 */
+
+	public static String getAnalysisRunFileLabel(Analysis analRun)
+	{
+		// String fileName = Globals.MODEL_OUTPUT_FILE_PREFIX +
+		// analRun.getAnalysisRunId() + Globals.ANALYSIS_RUN_FILE_PREFIX +
+		// analRun.getAnalysisId();
 		String fileName = Globals.ANALYSIS_RUN_FILE_PREFIX + analRun.getId();
-		return getUserDirPath(analRun.getAnalysable().getOwner()) + File.separator + fileName;
+		return getUserDirPath(analRun.getAnalysable().getOwner())
+				+ File.separator + fileName;
 	}
-	
-	public static File getAnalysisRunFilePDF(Analysis analRun) {
-		return new File(getAnalysisRunFileLabel(analRun) + Globals.PDF_FILE_SUFFIX);
+
+	public static File getAnalysisRunFilePDF(Analysis analRun)
+	{
+		return new File(getAnalysisRunFileLabel(analRun)
+				+ Globals.PDF_FILE_SUFFIX);
 	}
-	
-	public static File getAnalysisRunFileBenchPDF(Analysis analRun) {
-		return new File(getAnalysisRunFileLabelBench(analRun) + Globals.PDF_FILE_SUFFIX);
+
+	public static File getAnalysisRunFileBenchPDF(Analysis analRun)
+	{
+		return new File(getAnalysisRunFileLabelBench(analRun)
+				+ Globals.PDF_FILE_SUFFIX);
 	}
-	
-	public static File getAnalysisRunFilePNG(Analysis analRun) {
-		return new File(getAnalysisRunFileLabel(analRun) + Globals.PNG_FILE_SUFFIX);
+
+	public static File getAnalysisRunFilePNG(Analysis analRun)
+	{
+		return new File(getAnalysisRunFileLabel(analRun)
+				+ Globals.PNG_FILE_SUFFIX);
 	}
-	
-	public static File getAnalysisRunFileBenchPNG(Analysis analRun) {
-		return new File(getAnalysisRunFileLabelBench(analRun) + Globals.PNG_FILE_SUFFIX);
+
+	public static File getAnalysisRunFileBenchPNG(Analysis analRun)
+	{
+		return new File(getAnalysisRunFileLabelBench(analRun)
+				+ Globals.PNG_FILE_SUFFIX);
 	}
-	
-	public static File getAnalysisRunFileThumb(Analysis analRun) {
-		return new File(getAnalysisRunFileLabel(analRun) + Globals.THUMB_FILE_SUFFIX);
+
+	public static File getAnalysisRunFileThumb(Analysis analRun)
+	{
+		return new File(getAnalysisRunFileLabel(analRun)
+				+ Globals.THUMB_FILE_SUFFIX);
 	}
-	
+
 	public static String correctSlashesForR(String input)
 	{
-		if( !System.getProperty("file.separator").equals("/") )
+		if (!System.getProperty("file.separator").equals("/"))
 		{
-		    input = input.replace('/','\\');
+			input = input.replace('/', '\\');
 		}
-		String doubleBackslash = input.replace("\\","\\\\");
+		String doubleBackslash = input.replace("\\", "\\\\");
 		return doubleBackslash;
 	}
 
-	public static String getAnalysisRunFileLabelBench(Analysis analRun) 
+	public static String getAnalysisRunFileLabelBench(Analysis analRun)
 	{
-		String fileName = Globals.ANALYSIS_RUN_FILE_PREFIX_BENCH + analRun.getId();
-		return getUserDirPath(analRun.getAnalysable().getOwner()) + File.separator + fileName;
+		String fileName = Globals.ANALYSIS_RUN_FILE_PREFIX_BENCH
+				+ analRun.getId();
+		return getUserDirPath(analRun.getAnalysable().getOwner())
+				+ File.separator + fileName;
 	}
-
-	
 
 }

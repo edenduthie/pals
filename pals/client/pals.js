4a856d5315e13876320e55bf9c2cf0359fed7c6c
edenduthie <eduthie@gmail.com>
2013-02-11 05:37:10 +0000

    Added welcome message

--- pals/client/pals.js
+++ pals/client/pals.js
@@ -1,15 +1,6 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to pals.";
-  };
+contentSelector = '#main-content';
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
